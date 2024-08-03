a623119435c87666e7eba6aec3501c11a9704617
beckpalmx77 <beckpalmx@gmail.com>
2024-08-03 11:09:11 +0700

    Update random_lucky_draw.php

--- random_lucky_draw.php
+++ random_lucky_draw.php
@@ -4,7 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Random Winner</title>
+    <title>10 ปี สงวนออโต้คาร์</title>
+    <link rel="icon" href="img/favicon.ico" type="image/x-icon">
+    <link href="img/logo/logo.png" rel="icon">
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://cdn.jsdelivr.net/npm/tsparticles-preset-fireworks@2/tsparticles.preset.fireworks.bundle.min.js"></script>
     <style>
