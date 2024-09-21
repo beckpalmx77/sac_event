a1d4c414a04f17b3a0baaa4b448f9cb9a94ec79c
beckpalmx777 <beckpalmx@gmail.com>
2024-09-21 10:49:10 +0700

    update

--- login-bak.php
+++ login-bak.php
@@ -10,7 +10,9 @@ include('includes/CheckDevice.php');
     body {
         font-family: 'Prompt', sans-serif;
     }
+</style>
 
+<style type="text/css">
     .toggleeye {
         float: right;
         margin-right: 6px;
@@ -19,48 +21,19 @@ include('includes/CheckDevice.php');
         z-index: 2;
         color: darkgrey;
     }
-
-    /* การตอบสนองสำหรับสมาร์ทโฟน */
-    @media only screen and (max-width: 430px) {
-        .container-login {
-            padding: 15px;
-        }
-
-        .card {
-            margin: 0;
-            width: 100%;
-            box-shadow: none;
-        }
-
-        .login-form img {
-            width: 100%;
-            height: auto;
-        }
-
-        .login-form h1 {
-            font-size: 18px;
-        }
-
-        .form-group input {
-            font-size: 14px;
-        }
-
-        button.form-control.btn {
-            font-size: 16px;
-        }
-
-        /* ปรับขนาดข้อความ "Remember Me" */
-        .form-check-label p {
-            font-size: 12px;
-        }
-    }
 </style>
 
+
 <script>
+
     $(document).ready(function () {
-        let username = '<?php if (isset($_COOKIE["username"])) { echo $_COOKIE["username"]; } ?>';
-        let password = '<?php if (isset($_COOKIE["password"])) { echo $_COOKIE["password"]; } ?>';
-        let remember_chk = '<?php echo $_COOKIE["remember_chk"] ?>';
+        let username = '<?php if (isset($_COOKIE["username"])) {
+            echo $_COOKIE["username"];
+        } ?>';
+        let password = '<?php if (isset($_COOKIE["password"])) {
+            echo $_COOKIE["password"];
+        } ?>';
+        let remember_chk = '<?php echo $_COOKIE["remember_chk"]?>';
 
         $("#username").val(username);
         $("#password").val(password);
@@ -69,21 +42,20 @@ include('includes/CheckDevice.php');
             $("#remember").prop('checked', true); // Checked
         }
 
+    });
+
+</script>
+
+<script>
+    $(document).ready(function () {
         $("button").click(function () {
             check_login();
         });
-
-        $('#togglePassword').click(function () {
-            $('#password').attr('type') === 'password' ? $('#password').attr('type', 'text') : $('#password').attr('type', 'password');
-        });
-
-        $(document).keyup(function(event) {
-            if (event.which === 13) {
-                check_login();
-            }
-        });
     });
 
+</script>
+
+<script>
     function check_login() {
         let username = $("#username").val();
         let password = $("#password").val();
@@ -93,8 +65,9 @@ include('includes/CheckDevice.php');
             remember = $("#remember").val();
         }
 
-        if (username !== "" && password !== "") {
-            $.ajax({
+        if (username != "" && password != "") {
+            $.ajax
+            ({
                 type: 'post',
                 url: 'login_process.php',
                 data: {
@@ -119,7 +92,26 @@ include('includes/CheckDevice.php');
     }
 </script>
 
+
+<script type='text/javascript'>
+    $(document).ready(function () {
+        $('#togglePassword').click(function () {
+            //alert($(this).is(':checked'));
+            $('#password').attr('type') === 'password' ? $('#password').attr('type', 'text') : $('#password').attr('type', 'password');
+        });
+    });
+</script>
+
+<script>
+    $(document).keyup(function(event) {
+        if (event.which === 13) {
+            check_login();
+        }
+    });
+</script>
+
 <body class="bg-gradient-login">
+<!-- Login Content -->
 <div class="container-login">
     <div class="row justify-content-center">
         <div class="col-xl-6 col-lg-12 col-md-9">
@@ -129,36 +121,43 @@ include('includes/CheckDevice.php');
                         <div class="col-lg-12">
                             <div class="login-form">
                                 <div class="text-center">
-                                    <div>
-                                        <img src="img/logo/logo text-01.png" width="400" height="158" alt="Logo" />
-                                    </div>
+                                    <div><img src="img/logo/logo text-01.png" width="400" height="158"/></div>
                                     <h1 class="h4 text-gray-900 mb-4">ระบบจัดการ Event SAC</h1>
                                 </div>
                                 <div class="form-group">
-                                    <input type="text" class="form-control" id="username" placeholder="Enter User Name">
+                                    <input type="text" class="form-control" id="username"
+                                           value=""
+                                           placeholder="Enter User Name">
                                 </div>
                                 <div class="form-group">
-                                    <input type="password" class="form-control" id="password" placeholder="Password">
-                                    <span class="far fa-eye toggleeye" id="togglePassword" style="cursor: pointer;"></span>
+                                    <input type="password" class="form-control" id="password"
+                                           value=""
+                                           placeholder="Password">
+                                    <span class="far fa-eye toggleeye" id="togglePassword"
+                                          style="cursor: pointer;"></span>
                                 </div>
+
                                 <div class="form-group row">
                                     <div class="col-sm-12">
                                         <div class="form-check">
-                                            <input class="form-check-input" type="checkbox" value="on" id="remember" name="remember">
+                                            <input class="form-check-input" type="checkbox" value="on" id="remember"
+                                                   name="remember">
                                             <label class="form-check-label" for="remember">
-                                                <?php if ($_SESSION['deviceType'] == 'computer') { ?>
-                                                    <p style="color:blue;">Remember Me 30 Days</p>
+                                                 <?php  if ($_SESSION['deviceType']=='computer') {?>
+                                                <p style="color:blue;">Remember Me 30 Days</p>
                                                 <?php } else { ?>
-                                                    <p style="color:red;">Remember Me 30 Days</p>
+                                                <p style="color:red;">Remember Me 30 Days</p>
                                                 <?php } ?>
                                             </label>
                                         </div>
                                     </div>
                                 </div>
+
                                 <div class="form-group">
-                                    <button type="button" name="login-submit" id="login-submit" tabindex="4" class="form-control btn btn-primary">
-                                        <span class="spinner"><i class="icon-spin icon-refresh" id="spinner"></i></span> Log In
-                                    </button>
+                                    <button type="button" name="login-submit" id="login-submit" tabindex="4"
+                                            class="form-control btn btn-primary">
+                                            <span class="spinner">
+                                                <i class="icon-spin icon-refresh" id="spinner"></i></span> Log In
                                 </div>
                             </div>
                         </div>
@@ -168,5 +167,8 @@ include('includes/CheckDevice.php');
         </div>
     </div>
 </div>
+
 </body>
 </html>
+
+
